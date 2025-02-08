007f798967804425fdea594895198779362a92e8
fole <a_fole@protonmail.com>
2025-02-08 18:29:57 +0700

    test mobile

--- src/components/About/About.tsx
+++ src/components/About/About.tsx
@@ -22,35 +22,17 @@ const About = () => {
     return (
         <section className="about">
             <div className="about-container">
-                <div className="about-text">
-                    <h2 className="about-title">Welcome to Leixlip Dog Grooming</h2>
-                    <p className="about-description">
-                        Leixlip’s friendly and caring dog grooming salon!
-                        Founded in 2025, our salon is a passion project run by Sandra, who has always had a deep love
-                        for dogs.
-                    </p>
-                    <p className="about-description">
-                        With years of experience and a gentle touch, she treats every pup like her own, ensuring a
-                        stress-free and enjoyable grooming experience.
-                        Whether it's a simple wash or a full grooming session, we provide top-quality care tailored to
-                        each dog's needs.
-                    </p>
-                    <div className="hero-text">
-                        <div className="services-container">
-                            {services.map((service, index) => (
-                                <div className="service-card" key={index}>
-                                    <div className="service-icon">{service.icon}</div>
-                                    <h3 className="service-title">{service.title}</h3>
-                                    <p className="service-description">{service.description}</p>
-                                </div>
-                            ))}
-                        </div>
-                        <button className="hero-button">Book Now</button>
-                    </div>
-
-                </div>
                 <div className="about-image">
                     <img src="img.png" alt="A happy, freshly groomed dog"/>
+                </div>
+                <div className="services-container">
+                    {services.map((service, index) => (
+                        <div className="service-card" key={index}>
+                            <div className="service-icon">{service.icon}</div>
+                            <h3 className="service-title">{service.title}</h3>
+                            <p className="service-description">{service.description}</p>
+                        </div>
+                    ))}
                 </div>
             </div>
         </section>
