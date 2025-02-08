e03b6e730c16f530322747ef91ab38a89d7143a0
fole <a_fole@protonmail.com>
2025-02-08 14:52:55 +0700

    About me and Hero banner height optimization

--- src/components/About/About.tsx
+++ src/components/About/About.tsx
@@ -7,8 +7,8 @@ const About = () => {
                 <div className="about-text">
                     <h2 className="about-title">About Us</h2>
                     <p className="about-description">
-                        Welcome to [Your Salon Name], Leixlip’s friendly and caring dog grooming salon!
-                        Founded in 2025, our salon is a passion project run by my mom, who has always had a deep love for dogs.
+                        Welcome to Leixlip Dog Grooming, Leixlip’s friendly and caring dog grooming salon!
+                        Founded in 2025, our salon is a passion project run by Sandra, who has always had a deep love for dogs.
                     </p>
                     <p className="about-description">
                         With years of experience and a gentle touch, she treats every pup like her own, ensuring a stress-free and enjoyable grooming experience.
