bf67eb278f72ed84774fecd4988b3e438cce628a
fole <a_fole@protonmail.com>
2025-02-14 15:53:46 +0700

    services separation and style

--- src/components/Hero/Hero.tsx
+++ src/components/Hero/Hero.tsx
@@ -1,24 +1,6 @@
 import './Hero.css';
 import {Helmet} from "react-helmet-async";
 
-const services = [
-    {
-        title: 'One-on-One Grooming',
-        description: 'A complete grooming package including bath, haircut, and nail trimming ✂️.',
-        icon: '🛁',
-    },
-    {
-        title: 'Bath & Brush',
-        description: 'Pamper your pup with a luxurious bath and brushing to keep their coat shiny and clean.',
-        icon: '🧼',
-    },
-    {
-        title: 'De-Shedding Treatment',
-        description: 'Reduce shedding with our specialized de-shedding service for a healthier coat.',
-        icon: '🐾',
-    },
-];
-
 const jsonLd = {
     "@context": "https://schema.org",
     "@type": "LocalBusiness",
@@ -93,19 +75,6 @@ const Hero = () => {
                     </div>
                 </div>
             </header>
-
-            <section className="services-container">
-                <h2 className="hero-title">Our Services</h2>
-                <ul className="services-list">
-                    {services.map((service, index) => (
-                        <li className="service-card" key={index}>
-                            <div className="service-icon">{service.icon}</div>
-                            <h3 className="service-title">{service.title}</h3>
-                            <p className="service-description">{service.description}</p>
-                        </li>
-                    ))}
-                </ul>
-            </section>
         </>
     );
 };
--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,10 +1,12 @@
 import Hero from "../components/Hero/Hero.tsx";
 import Contact from "../components/Contact/Contact.tsx";
+import {Services} from "../components/Services/Services.tsx";
 
 export const Home = () => {
     return (
         <div>
             <section id="about"><Hero/></section>
+            <section id="about"><Services/></section>
             <section id="contact"><Contact/></section>
         </div>
     )
