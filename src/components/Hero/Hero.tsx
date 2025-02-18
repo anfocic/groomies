7fb8194f80cf0e94a65d644a726078773e13c651
fole <a_fole@protonmail.com>
2025-02-18 16:05:16 +0700

    lcp improvement

--- src/components/Hero/Hero.tsx
+++ src/components/Hero/Hero.tsx
@@ -54,14 +54,12 @@ const Hero = () => {
             <header className="hero">
                 <div className="hero-container">
                     <div className="hero-content">
-                        <div className="hero-header">  {/* New wrapper for title & logo */}
-                            <h1 className="hero-title">
-                                Welcome to<br/> Leixlip Dog Grooming
-                            </h1>
+                        <div className="hero-header">
+                            {/*<h1 className="hero-title">*/}
+                            {/*    Welcome to<br/> Leixlip Dog Grooming*/}
+                            {/*</h1>*/}
                             <div className="hero-image">
                                 <img src="leixlip-dog-grooming-logo.webp"
-                                     width="300"
-                                     height="299"
                                      alt="Leixlip Dog Grooming logo featuring two happy Pomeranians"/>
                             </div>
                         </div>
