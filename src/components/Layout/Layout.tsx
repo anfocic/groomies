21d0f1148d3dbb5dd0114bd6f7b02d1b01750f92
fole <a_fole@protonmail.com>
2025-02-19 12:47:08 +0700

    lazy loading

--- src/components/Layout/Layout.tsx
+++ src/components/Layout/Layout.tsx
@@ -1,13 +1,14 @@
 import Header from "../Header/Header.tsx";
-import Footer from "../Footer/Footer.tsx";
-import {FC, PropsWithChildren} from "react";
+import {FC, lazy, PropsWithChildren} from "react";
+
+const Footer = lazy(() => import("../Footer/Footer.tsx"));
 
 export const Layout: FC<PropsWithChildren> = ({children}) => {
-  return (
-      <div>
-          <Header/>
-          {children}
-          <Footer/>
-      </div>
-  )
+    return (
+        <div>
+            <Header/>
+            {children}
+            <Footer/>
+        </div>
+    )
 }
